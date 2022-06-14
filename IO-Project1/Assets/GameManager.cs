d256d686922deaceecdd7730f522e5f55e350257
Vivian <kaczuszka.zaglady@o2.pl>
2022-06-14 17:19:56 +0200

    Restart update

--- IO-Project1/Assets/GameManager.cs
+++ IO-Project1/Assets/GameManager.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public float delay = 1f;
     public void GameOver()
+    {
+        Invoke("Restart", delay);
+        
+    }
+
+    private void Restart()
     {
         SceneManager.LoadScene("Game01");
     }
