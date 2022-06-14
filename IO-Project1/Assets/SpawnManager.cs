c2209fcd0e2795315ac28e732cf652c1f0de434c
Vivian <kaczuszka.zaglady@o2.pl>
2022-06-14 13:43:30 +0200

    P1-7

--- IO-Project1/Assets/SpawnManager.cs
+++ IO-Project1/Assets/SpawnManager.cs
@@ -5,4 +5,37 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject [] spawnPoints = new GameObject [4];
+    public GameObject obstPrefab;
+
+    private float nextSpawnTime;
+    public float spawnDelay = 3f;
+
+    private void Update()
+    {
+        if (CanSpawn())
+        {
+            SpawnObstacle();
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        nextSpawnTime = Time.timeScale + spawnDelay;
+
+        int freeSpace = Random.Range(0, 3);
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            if(i == freeSpace) { continue; }
+            Instantiate(obstPrefab, spawnPoints[i].transform.position, spawnPoints[i].transform.rotation);
+        }
+        
+    }
+    
+    private bool CanSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+
+
 }
+
